9625289e6f70b5c8884e01f5b731ccff0483577d
Nabeel Ahmed <nabeel040604@gmail.com>
2026-04-15 22:30:01 +0500

    Updated frontend code

--- frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: "globalThis",   // required for ethers.js in browser
-  },
-});
